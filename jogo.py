1bac34410a1fee07f32c93356f85c4a857b8efc6
lionellpearl42-pixel <lionellpearl42@gmail.com>
2026-02-13 17:50:27 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -1,27 +1,35 @@
-import random
-from database import c
-from ai_groq import gerar_dica
+from ia import gerar_palavra_ia
 
-def iniciar_jogo():
-    c.execute("SELECT * FROM words ORDER BY RANDOM() LIMIT 1")
-    palavra, hint = c.fetchone()
-    jogo = {
+async def iniciar_jogo():
+    palavra, dica = await gerar_palavra_ia()
+
+    return {
         "palavra": palavra,
-        "dica": gerar_dica(palavra, hint),
+        "dica": dica,
+        "acertos": ["_" for _ in palavra],
         "tentativas": 6,
-        "acertos": ["_" for _ in palavra]
+        "letras_usadas": []
     }
-    return jogo
 
 def tentar_letra(jogo, letra):
-    letra = letra.lower()
+    if letra in jogo["letras_usadas"]:
+        return False, False
+
+    jogo["letras_usadas"].append(letra)
+
     if letra in jogo["palavra"]:
         for i, l in enumerate(jogo["palavra"]):
             if l == letra:
                 jogo["acertos"][i] = letra
-        venceu = "_" not in jogo["acertos"]
-        return True, venceu
+
+        if "_" not in jogo["acertos"]:
+            return True, True
+
+        return True, False
     else:
         jogo["tentativas"] -= 1
-        perdeu = jogo["tentativas"] == 0
-        return False, perdeu
+
+        if jogo["tentativas"] <= 0:
+            return False, True
+
+        return False, False
